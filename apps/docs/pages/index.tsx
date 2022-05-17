f3432690397e33b5491fd0824c9dcaf84558535f
George Carl <blackhxxdini@gmail.com>
2022-05-17 15:26:42 +0100

    refact(ui): landing pages

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,17 @@
+import Head from "next/head";
 import { Button } from "ui";
 
 export default function Docs() {
   return (
-    <div>
-      <h1>Docs</h1>
-      <Button />
-    </div>
+    <>
+      <Head>
+        <title>Documentation App</title>
+      </Head>
+
+      <div className="flex w-full min-h-screen justify-center items-center">
+        <h1 className="font-bold text-lg">Docs</h1>
+        <Button />
+      </div>
+    </>
   );
 }
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,19 @@
+import Head from "next/head";
 import { Button } from "ui";
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <>
+      <Head>
+        <title>Web App</title>
+      </Head>
+
+      <div className="flex w-full min-h-screen justify-center items-center">
+        <div className="flex space-x-2 items-center">
+          <h1 className="font-bold text-lg">Web</h1>
+          <Button />
+        </div>
+      </div>
+    </>
   );
 }
