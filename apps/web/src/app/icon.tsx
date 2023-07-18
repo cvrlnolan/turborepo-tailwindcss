5de04104702e9177330972d7dbe79172b945ff30
George Carl <blackhxxdini@gmail.com>
2023-07-18 14:19:43 +0100

    feat(ui): add app browser icons

---
+++ apps/web/src/app/icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/server';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 20,
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: '5%',
+        }}
+      >
+        W
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
